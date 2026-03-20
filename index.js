25018f8dcd9067c46f0dfccaae4b22c19ec7560f
unknown <meniar.mhir@cesjb.be>
2026-03-21 00:03:46 +0100

    donnees pokedex

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//Pokedex donnees
+const pokedex = [
+  { id: 1, name: "Bulbizarre", type: "plante", level: 5 },
+  { id: 4, name: "Salamèche", type: "feu", level: 5 },
+  { id: 7, name: "Carapuce", type: "eau", level: 5 },
+  { id: 25, name: "Pikachu", type: "electrik", level: 12 },
+  { id: 39, name: "Rondoudou", type: "fee", level: 8 },
+  { id: 52, name: "Miaouss", type: "normal", level: 9 },
+  { id: 133, name: "Evoli", type: "normal", level: 10 }
+];
